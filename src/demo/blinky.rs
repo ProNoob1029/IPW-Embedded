f98f9692046d919459bae63ac0d6f2c77735d7a3
ProNoob1029 <pronoob1029@gmail.com>
2024-08-15 13:12:32 +0300

    borroooooow

--- src/demo/blinky.rs
+++ src/demo/blinky.rs
@@ -1,13 +1,15 @@
 #![no_std]
 #![no_main]
 
+use cyw43::Control;
 use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::{bind_interrupts, Peripheral, Peripherals};
+use embassy_rp::dma::Channel;
+use embassy_rp::gpio::{Level, Output, Pin};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_24, PIN_25, PIN_29, PIO0};
+use embassy_rp::pio::{Instance, InterruptHandler, Pio, PioPin};
 use embassy_time::{Duration, Timer};
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
@@ -23,23 +25,29 @@ async fn cyw43_task(
     runner.run().await
 }
 
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
+async fn wifi(
+    spawner: &Spawner,
+    pin_23: PIN_23,
+    pin_25: PIN_25,
+    pio0: PIO0,
+    pin_24: PIN_24,
+    pin_29: PIN_29,
+    dma_ch0: DMA_CH0
+) -> Control {
     let fw = include_bytes!("../../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../../cyw43-firmware/43439A0_clm.bin");
 
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let mut pio = Pio::new(p.PIO0, Irqs);
+    let pwr = Output::new(pin_23, Level::Low);
+    let cs = Output::new(pin_25, Level::High);
+    let mut pio = Pio::new(pio0, Irqs);
     let spi = PioSpi::new(
         &mut pio.common,
         pio.sm0,
         pio.irq0,
         cs,
-        p.PIN_24,
-        p.PIN_29,
-        p.DMA_CH0,
+        pin_24,
+        pin_29,
+        dma_ch0,
     );
 
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
@@ -51,6 +59,25 @@ async fn main(spawner: Spawner) {
     control
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
         .await;
+
+    control
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let peripherals = embassy_rp::init(Default::default());
+
+    let mut control = wifi(
+        &spawner,
+        peripherals.PIN_23,
+        peripherals.PIN_25,
+        peripherals.PIO0,
+        peripherals.PIN_24,
+        peripherals.PIN_29,
+        peripherals.DMA_CH0
+    ).await;
+
+    let mut led_pin = Output::new(peripherals.PIN_0, Level::Low);
 
     let delay = Duration::from_millis(50);
     loop {
