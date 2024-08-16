4658601e8c79948b3f6851ac0a76e09f4ca30f0c
ProNoob1029 <pronoob1029@gmail.com>
2024-08-16 13:04:31 +0300

    i2c scan

--- src/exercises/ex2.rs
+++ src/exercises/ex2.rs
@@ -3,65 +3,39 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_futures::select::{select4, Either4};
-use embassy_rp::gpio::{Input, Pull};
-use rgb::RgbLed;
+use embassy_rp::bind_interrupts;
+use embassy_rp::i2c::{Config as I2cConfig, I2c, InterruptHandler as I2CInterruptHandler};
+use embassy_rp::peripherals::I2C0;
 #[allow(unused_imports)]
 use {defmt_rtt as _, panic_probe as _};
-
-mod rgb;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let peripherals = embassy_rp::init(Default::default());
 
-    let mut rgb: RgbLed = RgbLed::new(
-        peripherals.PWM_SLICE0,
-        peripherals.PWM_SLICE1,
-        peripherals.PIN_0,
-        peripherals.PIN_1,
-        peripherals.PIN_2
-    );
+    let sda = peripherals.PIN_20;
+    let scl = peripherals.PIN_21;
 
-    let mut button_a = Input::new(peripherals.PIN_12, Pull::Up);
-    let mut button_b = Input::new(peripherals.PIN_13, Pull::Up);
-    let mut button_x = Input::new(peripherals.PIN_14, Pull::Up);
-    let mut button_y = Input::new(peripherals.PIN_15, Pull::Up);
+    let mut i2c = I2c::new_async(peripherals.I2C0, scl, sda, Irqs, I2cConfig::default());
+
+    let mut rx_buf = [0x00u8; 2];
+
+    info!("Scanning addresses");
+
+    for addr in 0..127_u16 {
+        match i2c.read_async(addr, &mut rx_buf).await {
+            Ok(_) => {
+                info!("Successful read on address 0x{:02x}", addr)
+            }
+            Err(_) => {}
+        }
+    }
 
     loop {
-        let select_button = select4(
-            button_a.wait_for_falling_edge(),
-            button_b.wait_for_falling_edge(),
-            button_x.wait_for_falling_edge(),
-            button_y.wait_for_falling_edge()
-        ).await;
 
-        match select_button {
-            Either4::First(_) => {
-                rgb.set_green(0.2392);
-                rgb.set_red(1.0);
-                rgb.set_blue(0.8);
-                info!("PINK!");
-            }
-            Either4::Second(_) => {
-                rgb.set_red(0.0);
-                rgb.set_blue(1.0);
-                rgb.set_green(1.0);
-                info!("CYAN!");
-            }
-            Either4::Third(_) => {
-                rgb.set_blue(0.0);
-                rgb.set_green(1.0);
-                rgb.set_red(1.0);
-                info!("YELLOW!");
-            }
-            Either4::Fourth(_) => {
-                rgb.set_blue(1.0);
-                rgb.set_green(1.0);
-                rgb.set_red(1.0);
-                info!("WHITE!");
-            }
-        }
     }
 }
 
+bind_interrupts!(struct Irqs {
+    I2C0_IRQ => I2CInterruptHandler<I2C0>;
+});
